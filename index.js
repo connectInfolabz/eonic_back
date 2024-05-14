331000920cae7591f53c159486b738528aa4f4e5
connectInfolabz <connectinfolabz@gmail.com>
2024-05-14 16:38:33 +0530

    first commit

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(
 );
 app.use(
     cors({
-        origin: ["http://localhost:3001", "http://localhost:3000"],
+        origin: ["http://localhost:3001", "http://localhost:3000", process.env.CLIENT_URL],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
